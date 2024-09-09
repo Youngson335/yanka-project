e09fa010f10a811aea5bb3d28ce98f3cc15ba2de
Владислав <tolstovvvlad335@gmai.com>
2024-09-09 20:39:36 +0900

    test development

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,62 @@
 <template>
-  <HomePage />
+  <transition name="bounce">
+    <div class="app__description" v-if="showDescription">
+      <Description />
+    </div>
+  </transition>
+  <HomePage v-if="!showDescription" />
+  <div class="movie__back">
+    <video src="./assets/background/back2.mp4" autoplay muted loop></video>
+  </div>
   <router-view />
 </template>
 
 <script>
 import HomePage from "./pages/HomePage.vue";
+import Description from "./components/Modals/Description.vue";
+import { mapGetters } from "vuex";
 export default {
   components: {
     HomePage,
+    Description,
+  },
+  data() {
+    return {
+      showDescription: true,
+    };
+  },
+  computed: {
+    ...mapGetters(["getConfirmAccept"]),
+  },
+  watch: {
+    getConfirmAccept: {
+      immediate: true,
+      handler(newValue) {
+        this.checkConfirmAccept(newValue);
+      },
+    },
+  },
+  methods: {
+    checkConfirmAccept(value) {
+      this.showDescription = !value;
+    },
+  },
+
+  mounted() {
+    this.checkConfirmAccept(this.getConfirmAccept);
   },
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Comfortaa", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #f4f0f0;
+  background-color: rgb(0 0 0 / 45%);
+  height: 100vh;
 }
 * {
   padding: 0;
@@ -26,12 +64,70 @@ export default {
   box-sizing: border-box;
 }
 body {
-  background-color: white;
+  overflow: hidden;
 }
 .container {
   max-width: 600px;
   width: 100%;
   margin: 0 auto;
-  padding: 10px;
+  padding: 20px;
+}
+.movie__back {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: -1;
+  animation: hiddenBlur 3s ease;
+  & video {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    transition: all 0.3s ease;
+  }
+  @keyframes hiddenBlur {
+    0% {
+      filter: blur(3px);
+    }
+    100% {
+      filter: blur(0px);
+    }
+  }
+}
+.app__description {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.bounce-enter-active,
+.bounce-leave-active {
+  animation: hiddenModalDescription 1s ease;
+}
+
+.bounce-enter,
+.bounce-leave-to {
+  transform: translateY(0);
+  transition: all 1s ease;
+  opacity: 0;
+}
+
+@keyframes hiddenModalDescription {
+  0% {
+    transform: translateY(0px);
+  }
+  30% {
+    transform: translateY(-10px);
+  }
+  100% {
+    transform: translateY(100px);
+  }
 }
 </style>
---
+++ src/components/Modals/Description.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="description">
+    <div class="description__text">
+      <p>
+        Данное приложение доступно не всем и то, что вы сейчас можете им
+        пользоваться - это большая честь в первую очередь для автора!
+      </p>
+      <p>
+        И ему так же хочется убедиться в том, что вы готовы уделять свое
+        внимание, пользуясь данным приложением!
+      </p>
+      <p>Будет прекрасно, если вы это подтвердите чуть ниже!)</p>
+    </div>
+    <div class="description__form">
+      <AcceptAgreement />
+    </div>
+    <div class="description__button">
+      <ConfirmBtn />
+    </div>
+  </div>
+</template>
+<script>
+import ConfirmBtn from "../Buttons/ConfirmBtn.vue";
+import AcceptAgreement from "../AcceptAgreement.vue";
+import { mapGetters } from "vuex";
+export default {
+  components: {
+    ConfirmBtn,
+    AcceptAgreement,
+  },
+};
+</script>
+<style lang="scss">
+.description {
+  width: 100%;
+  max-width: 350px;
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+  color: rgba(0, 0, 0, 0.5803921569);
+  animation: showDescriptionBlock 1s ease;
+  &__text {
+    p {
+      margin-bottom: 10px;
+      text-align: start;
+    }
+  }
+  @keyframes showDescriptionBlock {
+    0% {
+      transform: translateY(20px);
+      filter: blur(1px);
+      opacity: 0;
+    }
+    100% {
+      transform: translateY(0px);
+      filter: blur(0px);
+      opacity: 1;
+    }
+  }
+}
+</style>
