ca076e01a5b2abf3810227ea841f802153f8264d
Владислав <a1@MacBook-Pro-1.local>
2025-01-15 21:56:40 +0900

    поставил заглушку

--- src/audio-scripts/vibrate.js
+++
@@ -1,9 +0,0 @@
-function vibrateDevice(duration) {
-  if (navigator.vibrate) {
-    navigator.vibrate(duration);
-  } else {
-    console.warn("Вибрация не поддерживается на этом устройстве.");
-  }
-}
-
-export default vibrateDevice;
